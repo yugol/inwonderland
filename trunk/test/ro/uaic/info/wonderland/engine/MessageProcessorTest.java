bdea0e930d038c69687597c1bdae2445092a51e0
iulian.goriac <iulian.goriac@dc13d4f6-1963-11df-85bb-b9a83fee2cb1>
2010-02-21 23:47:29 +0000

    FUTURE CONTINUOUS
    Results: 42 error(s), for 5874 words in 949 sentences.

--- trunk/test/ro/uaic/info/wonderland/engine/MessageProcessorTest.java
+++ trunk/test/ro/uaic/info/wonderland/engine/MessageProcessorTest.java
@@ -48,7 +48,7 @@ public class MessageProcessorTest {
 
     @Test
     public void testMany() throws Exception {
-        int from = 885;
+        int from = 947;
         int to = 0;
 
         List<String> lines = IO.getFileContentAsStringList(new File(Globals.getCorporaFolder(), "egcp.train.plain.txt"));
