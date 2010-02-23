9b07159fe1e0a924ef8595fe766322e26cef4aff
iulian.goriac <iulian.goriac@dc13d4f6-1963-11df-85bb-b9a83fee2cb1>
2010-02-23 08:52:00 +0000

    Projection support

---
+++ src/ro/uaic/info/wonderland/kb/transformations/DefaultTransformation.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ro.uaic.info.wonderland.kb.transformations;
+
+import fr.lirmm.rcr.cogui2.kernel.model.CGraph;
+import fr.lirmm.rcr.cogui2.kernel.model.KnowledgeBase;
+import fr.lirmm.rcr.cogui2.kernel.model.Projection;
+import java.util.List;
+
+/**
+ *
+ * @author Iulian
+ */
+public class DefaultTransformation implements ITransformation {
+
+    private KnowledgeBase kb;
+    private CGraph lhs;
+    private int id;
+    private List<Projection> projections;
+
+    public void init(KnowledgeBase kb, CGraph lhs, int id) {
+        this.kb = kb;
+        this.lhs = lhs;
+        this.id = id;
+    }
+
+    public int compare(ITransformation o1, ITransformation o2) {
+        Integer id1 = new Integer(o1.getId());
+        return id1.compareTo(o2.getId());
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public CGraph getLhs() {
+        return lhs;
+    }
+
+    public void setProjections(List<Projection> projections) {
+        this.projections = projections;
+    }
+}
---
+++ test/ro/uaic/info/wonderland/kb/CGT_Test1.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ro.uaic.info.wonderland.kb;
+
+import ro.uaic.info.wonderland.kb.transformations.DefaultTransformation;
+
+/**
+ *
+ * @author Iulian
+ */
+public class CGT_Test1 extends DefaultTransformation {
+}
