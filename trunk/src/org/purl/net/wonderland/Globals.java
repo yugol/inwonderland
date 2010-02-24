6437b8d3d83c865a0c37804efd7a680522ffedda
iulian.goriac <iulian.goriac@dc13d4f6-1963-11df-85bb-b9a83fee2cb1>
2010-02-24 21:39:45 +0000

    License

--- trunk/src/org/purl/net/wonderland/Globals.java
+++ trunk/src/org/purl/net/wonderland/Globals.java
@@ -1,11 +1,33 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *  The MIT License
+ *
+ *  Copyright 2010 Iulian Goriac <iulian.goriac@gmail.com>.
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
  */
 package org.purl.net.wonderland;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -13,15 +35,30 @@ import java.io.IOException;
  */
 public final class Globals {
 
+    static {
+        Properties projProp = new Properties();
+        try {
+            String[] paths = System.getProperty("java.class.path").split(";");
+            for (String path : paths) {
+                System.out.println(path);
+            }
+            // projProp.load(ClassLoader.getSystemResourceAsStream("wonderland.properties"));
+            // projProp.list(System.out);
+        } catch (Exception ex) {
+            System.err.println("Could not load properties file.");
+            System.err.println(ex);
+            exit();
+        }
+    }
     public static boolean testDebug = false;
-    private static File dataFolder = new File("./res/");
+    private static File resFolder = new File("./res/");
     public static boolean useMorphAdornerTagsInWordForm = false;
 
     public static String getResFolder() {
         String dataPath = System.getProperty("wonderland.data.path");
         if (dataPath == null) {
             try {
-                dataPath = dataFolder.getCanonicalPath();
+                dataPath = resFolder.getCanonicalPath();
             } catch (IOException ex) {
                 System.err.println("Error reading the data folder.");
                 System.err.println(ex);
@@ -53,7 +90,7 @@ public final class Globals {
     }
 
     public static File getCorporaFolder() {
-        return new File(dataFolder, "corpora");
+        return new File(resFolder, "corpora");
     }
 
     public static File getMorphologyFolder() {
