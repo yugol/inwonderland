132524b0695d5d97af879a8ce98e7313467f5bb9
yugol <yugo.lirianac@gmail.com>
2015-03-31 23:04:45 +0300

    MG Sampling Runner

---
+++ AiBrokerXP/marketglory-sample.bat
@@ -0,0 +1,11 @@
+rem in TaskScheduler set
+rem Program/script: cmd
+rem Add arguments: /K  F:\AiBrokerXP\marketglory-sample.bat
+rem Start in: F:\AiBrokerXP
+
+rem @echo off
+echo Updating EOD REGS
+set AIBROKERXP_HOME=%~dp0
+set JAVA="java.exe"
+cd "%AIBROKERXP_HOME%"
+%JAVA% -cp aibroker.jar ess.mg.driver.MGWebReader
